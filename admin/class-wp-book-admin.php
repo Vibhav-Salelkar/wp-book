aaa1af7910700792e2a1d98021325916079238e3
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-12 16:20:18 +0530

    feat2: creates custom hierarchical taxonomy

--- admin/class-wp-book-admin.php
+++ admin/class-wp-book-admin.php
@@ -114,4 +114,30 @@ class Wp_Book_Admin {
 		);
 	}
 
+	public function wb_register_taxonomy_book() {
+		$labels = array(
+			'name'              => _x( 'Book Category', 'taxonomy general name' ),
+			'singular_name'     => _x( 'Book', 'taxonomy singular name' ),
+			'search_items'      => __( 'Search Book Categories' ),
+			'all_items'         => __( 'All Book Categories' ),
+			'parent_item'       => __( 'Parent Book Category' ),
+			'parent_item_colon' => __( 'Parent Book Category:' ),
+			'edit_item'         => __( 'Edit Book Category' ),
+			'update_item'       => __( 'Update Book Category' ),
+			'add_new_item'      => __( 'Add New Book Category' ),
+			'new_item_name'     => __( 'New Book Category Name' ),
+			'menu_name'         => __( 'Book' ),
+		);
+		$args   = array(
+			'hierarchical'      => true, // make it hierarchical (like categories)
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_in_rest'      => true,    //required to see taxonomy in ui
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => [ 'slug' => 'book' ],
+		);
+		register_taxonomy( 'book', [ 'book' ], $args );
+	}
+
 }
